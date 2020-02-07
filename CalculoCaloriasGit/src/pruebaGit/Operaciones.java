c194bf1e184f9a9b34e34350df535b9bd83419d7
aless <poweredalex18@gmail.com>
2020-02-07 18:46:13 +0100

    Agregada la clase operaciones

--- CalculoCaloriasGit/src/pruebaGit/Operaciones.java
+++ CalculoCaloriasGit/src/pruebaGit/Operaciones.java
@@ -8,26 +8,5 @@ public class Operaciones {
 		this.operadorB = operadorB;
 	}
 	
-	public int getOperadorA() {
-		return operadorA;
-	}
 
-	public void setOperadorA(int operadorA) {
-		this.operadorA = operadorA;
-	}
-
-	public int getOperadorB() {
-		return operadorB;
-	}
-
-	public void setOperadorB(int operadorB) {
-		this.operadorB = operadorB;
-	}
-	
-	public int suma() {
-		return operadorA + operadorB;
-	}
-	public int resta() {
-		return operadorA - operadorB;
-	}
 }
