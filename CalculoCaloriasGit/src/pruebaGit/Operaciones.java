45c152a21eb351e9c24245a0b6fb05fbfef3914a
aless <poweredalex18@gmail.com>
2020-02-07 18:42:57 +0100

    Agregadas las funciones suma y resta

---
+++ CalculoCaloriasGit/src/pruebaGit/Operaciones.java
@@ -0,0 +1,33 @@
+package pruebaGit;
+
+public class Operaciones {
+	int operadorA, operadorB;
+
+	public Operaciones(int operadorA, int operadorB) {
+		this.operadorA = operadorA;
+		this.operadorB = operadorB;
+	}
+	
+	public int getOperadorA() {
+		return operadorA;
+	}
+
+	public void setOperadorA(int operadorA) {
+		this.operadorA = operadorA;
+	}
+
+	public int getOperadorB() {
+		return operadorB;
+	}
+
+	public void setOperadorB(int operadorB) {
+		this.operadorB = operadorB;
+	}
+	
+	public int suma() {
+		return operadorA + operadorB;
+	}
+	public int resta() {
+		return operadorA - operadorB;
+	}
+}
