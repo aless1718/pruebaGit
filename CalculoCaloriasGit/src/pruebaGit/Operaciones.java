f76702cd4d1279ff423ee2a2cbea35b2cceb83b3
aless <poweredalex18@gmail.com>
2020-02-07 19:25:45 +0100

    Operación división agregada

--- CalculoCaloriasGit/src/pruebaGit/Operaciones.java
+++ CalculoCaloriasGit/src/pruebaGit/Operaciones.java
@@ -8,5 +8,31 @@ public class Operaciones {
 		this.operadorB = operadorB;
 	}
 	
+	public int getOperadorA() {
+		return operadorA;
+	}
 
+	public void setOperadorA(int operadorA) {
+		this.operadorA = operadorA;
+	}
+
+	public int getOperadorB() {
+		return operadorB;
+	}
+
+	public void setOperadorB(int operadorB) {
+		this.operadorB = operadorB;
+	}
+	
+	public int suma() {
+		return operadorA + operadorB;
+	}
+	public int resta() {
+		return operadorA - operadorB;
+	}
+	//LA DE MULTIPLICACIÓN ME LA ELIMINASTE TÚ PORQUE DABA CONFLICTO
+	
+	public int division() {
+		return operadorA / operadorB;
+	}
 }
